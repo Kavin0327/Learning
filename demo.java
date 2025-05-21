ad6b25c62727d823cdf1fdde253953e4bf6042dd
Kavin_K <k.kavincse@gmail.com>
2025-05-21 14:59:29 +0530

    demo.java is modified in local machine in main

--- demo.java
+++ demo.java
@@ -1,6 +1,5 @@
 public class demo {
     public static void main(String[] args) {
-        System.out.println("Kavin");
-        System.out.println("K");
+        System.out.println("Kavin K");
     }
 }
